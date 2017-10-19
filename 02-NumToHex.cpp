0fc55aa87aa0d4aca44930b3e498274e828c6ce7
Tim Paduraru <timopaduraru@gmail.com>
2017-10-18 21:15:01 -0700

    streamlined 02

--- 02-NumToHex.cpp
+++ 02-NumToHex.cpp
@@ -12,29 +12,12 @@ std::string dec_to_hex(unsigned int dec)
 	{
 		int h = dec % 16;
 		char letter;
-		switch (h)
-		{
-			case 15:
-				letter = 'F';
-				break;
-			case 14:
-				letter = 'E';
-				break;
-			case 13: 
-				letter = 'D';
-				break;
-			case 12:
-				letter = 'C';
-				break;
-			case 11:
-				letter = 'B';
-				break;
-			case 10:
-				letter = 'A';
-				break;
-			default:
-				letter = h+48;
-		}
+		
+		if ( h > 9 )
+			letter = h+55;
+		else
+			letter = h+48;
+
 		hex = letter + hex;
 	}
 	
