f6e238f452ed0d61db702bbcd83671263d84652b
Tim Paduraru <timopaduraru@gmail.com>
2017-10-18 20:58:01 -0700

    made the input int unisigned

--- 01-NumInBin.cpp
+++ 01-NumInBin.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
+
+std::string dec_to_bin(unsigned int dec)
+{
+	if (!dec)
+		return std::to_string(0);
+	
+	std::string bin = "";
+	
+	for (; dec; dec /= 2 )
+		bin = std::to_string(dec % 2) + bin;
+	
+	return bin;
+}
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	unsigned int num;
+	
+	std::cout << "Enter a number: ";
+	std::cin >> num;
+	std::cout << num << " in binary is " << dec_to_bin(num) << std::endl;
+	
 	return 0;
 }
